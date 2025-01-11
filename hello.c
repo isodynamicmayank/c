bdb75ca2382e0d9589827323eea6181abf590a8d
isodynamicmayank <akashpopit82@gmail.com>
2025-01-11 18:51:22 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
  /*
- PURPOSE:To learn about the concept of magic numbers and implemnet same in c
+ PURPOSE:To learn about the concept of magic numbers and implement the same in c
  AUTHOR:AKASH SINHA
- DATE:2025-01-10
- OBJECTIVE:Find all magical numbers between 1 to 100
- PROBLEM STATEMENT:WRITE A PROGRAM IN C TO PRINT MAGIC NUMBERS FROM 1-100
+ DATE:2025-01-11
+ OBJECTIVE:Find magical number
+ PROBLEM STATEMENT:WRITE A PROGRAM IN C TO PRINT IF A NUMBER IS MAGICAL OR NOT
  */
 
 /* MAGIC NUMBER :A number is said to be magical number if the sum of digit is equal to "1",for example-163  -> 1+6+3->10->1+0->1
@@ -20,7 +20,9 @@ int sum_of_digits(int n)
 }  
   int main() 
   {
-        int num =64;
+        int num ;
+        printf("provide input integer");
+        scanf("%d",&num);
         int summation =0;
         
         summation =sum_of_digits(num);
